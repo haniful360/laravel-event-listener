0e73ef13ec913adbf25b844174b836bcc9c6d8cb
haniful360 <haniful360@gmail.com>
2024-12-22 12:22:57 +0600

    email send in each user inbox

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 
@@ -15,12 +16,23 @@ class PostController extends Controller
             'content' => $request->content
         ]);
 
+        // notify admin
         // send to mail plain text
         Mail::raw("A blog post '{$post->title}' have been published", function ($message) {
             $message->to('recipient@example.com')
                 ->subject('New Blog Post Published');
         });
 
+        // notify all user
+
+        $users = User::all();
+        foreach($users as $user){
+            Mail::raw("Hey '{$user->name}', checkout your new post: {$post->title}", function($message) use ($user){
+                $message->to($user->email)
+                        ->subject('New Blog post!');
+            });
+        }
+        
         return back();
     }
 }
